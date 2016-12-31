00531267e833b8cae82259ce4ff4f7d99753b4b4
pengdonglin <pengdonglin137@163.com>
2016-12-31 23:00:15 +0800

    Add some files.
    Signed-off-by: pengdonglin <pengdonglin137@163.com>

---
+++ mk.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+make uImage -j4
+
+make dtbs
+
+cp arch/arm/boot/uImage /tftpboot
+cp arch/arm/boot/dts/s3c2440-tq2440-dt.dtb  /tftpboot/dtb
+
+dtc -I dtb -O dts -o tq2440.dts arch/arm/boot/dts/s3c2440-tq2440-dt.dtb
